f974d288475902a9c63c570434728dcd4593aaec
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-10-09 19:43:39 -0400

    Update HW2_4.21.py

--- HW2_4.21.py
+++ HW2_4.21.py
@@ -1,8 +1,15 @@
+#! /usr/bin/python3
 import math
+ 
+n = 10#The Month in numerical form
+k = 2021 #year of the century
+j = 2021
+q = 10# day of the month
 
-q = int(input("Day of the month")) 
-m = int(input("Month"))
-j = float(input("Century"))
-k = int(input("Year of the century"))
-h = int(q+math.floor(26*(m+1)/10)+k+math.floor(k/4)+math.floor(j/4)+5*(j))%7
-print(h)
+"h = int(q+math.floor((26(n+1)/10))+k+math.floor(k/4)+math.floor(j/4)+5j)%7"
+base = (26*(n+1))/10
+base2 = k/4
+base3 = j/4
+base4 = 5*j
+form2 = (q + base+k+base2+base3+base4)%7
+print(int(form2))
