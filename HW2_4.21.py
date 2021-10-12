21b9858824d67b5e1df778007508a59f7b57d1d4
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-10-11 20:26:28 -0400

    Update HW2_4.21.py

--- HW2_4.21.py
+++ HW2_4.21.py
@@ -1,11 +1,10 @@
 #! /usr/bin/python3
 import math
- 
-n = 10#The Month in numerical form
-k = 2021 #year of the century
-j = 2021
-q = 10# day of the month
 
+n = int(input("Enter the month: ")) 
+k = int(input("Enter the year: "))
+q = int(input("Enter day of the Month:"))
+j = 2021
 "h = int(q+math.floor((26(n+1)/10))+k+math.floor(k/4)+math.floor(j/4)+5j)%7"
 base = (26*(n+1))/10
 base2 = k/4
@@ -13,3 +12,4 @@ base3 = j/4
 base4 = 5*j
 form2 = (q + base+k+base2+base3+base4)%7
 print(int(form2))
+
